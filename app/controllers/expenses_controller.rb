971c6e8d0e6d73aa3ab14e57dbf220426ecc2c31
Luis Felipe Bertel M <ellipe@gmail.com>
2016-09-18 15:32:07 -0500

    added filtering

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,6 +1,16 @@
 class ExpensesController < ApplicationController
   before_action :authenticate_user!
   def index
-     @expenses = Expense.order("date DESC")
+     @user = current_user
+     @expenses = @user.expenses.order("date DESC")
+
+     if !params[:category_id].blank?
+       @expenses = @expenses.where(category_id: params[:category_id])
+     end
+
+     if !params[:concept].blank?
+       @expenses = @expenses.where("concept like :query", query: "%#{params[:concept]}%")
+     end
+
   end
 end
